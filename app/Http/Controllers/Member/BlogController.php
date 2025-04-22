b2ae900013d19315259bac9d361c49a7667ccaf5
ikhsan <ikhsancahyamardika@gmail.com>
2025-04-22 16:47:10 +0700

    feat(dashboard page): display post data

---
+++ app/Http/Controllers/Member/BlogController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Member;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class BlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //menampilkan data berdasarkan user yg login
+        $user = Auth::user();
+        $postData = Post::where('user_id',$user->id)
+                ->orderBy('id', 'desc')
+                ->get();
+        
+        //menampilkan data di view blade     
+
+        return view('member.blogs.index', compact('postData'));
+    
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
